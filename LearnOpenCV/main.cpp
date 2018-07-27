cd32bbd16fb079ff7f07ace414315cea4e22922e
pomelyu <pomelyu5199@icloud.com>
2018-07-27 21:46:05 +0800

    Initial Commit

---
+++ LearnOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenCV
+//
+//  Created by Chien Chin-yu on 2018/7/27.
+//  Copyright © 2018 Chien Chin-yu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
