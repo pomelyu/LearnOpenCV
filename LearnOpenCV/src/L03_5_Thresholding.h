9a2b3f3f580b2fd6f34b610b5e9128e348b31754
pomelyu <pomelyu5199@icloud.com>
2018-08-02 23:28:34 +0800

    :sparkles: show otsu threshold result

--- LearnOpenCV/src/L03_5_Thresholding.h
+++ LearnOpenCV/src/L03_5_Thresholding.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+#include "cvplot.h"
 #include "utils.hpp"
 #include "cv/threshold.hpp"
 
@@ -43,7 +44,7 @@ void ch03_5_thresholding() {
     Mat thresOTSU;
     // OTSU algorithm is one of global thresholding,
     // which tries to find the threshold which could minize the variance among black & white part
-    threshold(image, thresOTSU, 127, 255, CV_THRESH_BINARY|CV_THRESH_OTSU);
+    double otsu_value = threshold(image, thresOTSU, 127, 255, CV_THRESH_BINARY|CV_THRESH_OTSU);
     
     Mat thresMean;
     adaptiveThreshold(image, thresMean, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 11, 2);
@@ -57,6 +58,51 @@ void ch03_5_thresholding() {
         ShowImages("Adaptive thresholding", subtitles, images, {2, 3}, 300, false);
     }
     
+    // Show OTSU result
+    {
+        Mat hist;
+        int histSize = 256;
+        float colorRanges[] = {0, 255};
+        const float* ranges[] = { colorRanges };
+        calcHist(&image, 1, 0, Mat(), hist, 1, &histSize, ranges, true, false);
+        
+        int FRAME_SIZE = 300;
+        vector<pair<float, float>> data;
+        cvplot::Window::current("OTSU threshold").offset({60, 100});
+        
+        {
+            auto name = "image";
+            cvplot::setWindowTitle(name, "image");
+            cvplot::moveWindow(name, 0, 0);
+            cvplot::resizeWindow(name, FRAME_SIZE, FRAME_SIZE);
+            auto &view = cvplot::Window::current().view(name);
+            Mat converted;
+            cvtColor(image, converted, CV_GRAY2BGR);
+            view.drawImage(&converted);
+            view.finish();
+        }
+        
+        {
+            auto name = "gray hist";
+            cvplot::setWindowTitle(name, "intensity histogram");
+            cvplot::moveWindow(name, FRAME_SIZE, 0);
+            cvplot::resizeWindow(name, FRAME_SIZE, FRAME_SIZE);
+            auto &figure = cvplot::figure(name);
+            for (int i = 0; i < 256; i++) { data.push_back({i, hist.at<float>(i)}); }
+            figure.series("intensity").set(data).type(cvplot::Histogram).color(cvplot::Color(0, 0, 0, 127));
+            
+            double maxValue;
+            minMaxLoc(hist, 0, &maxValue);
+            data.clear();
+            data.push_back({ otsu_value, 0 });
+            data.push_back({ otsu_value, maxValue});
+            figure.series("otsu threshold").set(data).type(cvplot::Line).color(cvplot::Blue);
+            figure.show(true);
+        }
+        waitKey();
+    }
+    
+    // Implementation result
     Mat myMean;
     meanBinaryThreshold(image, myMean, 11, 2);
     Mat myGaussian;
